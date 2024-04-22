38ba84032b9c7d823fad293e8e3df3fd8a955ff0
ekzofallendreamer <theskeletrongame@gmail.com>
2024-04-22 11:48:13 +0700

    task4

--- vue-project/js/main.js
+++ vue-project/js/main.js
@@ -23,11 +23,15 @@ let app = new Vue({
          ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0,
-        methods: {
-            addToCart() {
-                this.cart += 1
-            }
-         }         
-    }
+              
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
+     }   
  })
  
