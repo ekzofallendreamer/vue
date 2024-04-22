eb209ff597ab6d2ff98c49606c2eab3819399194
ekzofallendreamer <theskeletrongame@gmail.com>
2024-04-22 10:38:35 +0700

    final task 2

--- vue-project/js/main.js
+++ vue-project/js/main.js
@@ -3,7 +3,8 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: "A pair of warm, fuzzy socks"
+        altText: "A pair of warm, fuzzy socks",
+        image: "./assets/vmSocks-blue-onWhite.jpg"
     }
  })
  
