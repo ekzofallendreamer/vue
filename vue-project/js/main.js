ca5fdda8bb80c8679b7891d7dfefdb39be39d758
ekzofallendreamer <theskeletrongame@gmail.com>
2024-04-22 11:21:58 +0700

    final task 3

--- vue-project/js/main.js
+++ vue-project/js/main.js
@@ -6,7 +6,8 @@ let app = new Vue({
         altText: "A pair of warm, fuzzy socks",
         image: "./assets/vmSocks-blue-onWhite.jpg",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: true
+        inStock: true,
+        onSale: true
     }
  })
  
