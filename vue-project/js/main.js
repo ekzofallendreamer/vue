fd0c4e7ecdf8f73c65e9e356c4fc2e1a7f92ddca
ekzofallendreamer <theskeletrongame@gmail.com>
2024-04-23 19:59:50 +0700

    final task 8

--- vue-project/js/main.js
+++ vue-project/js/main.js
@@ -3,35 +3,50 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
         altText: "A pair of warm, fuzzy socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: false,
-        onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 0
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 10
             }
          ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-        cart: 0,
-              
+        cart: 0,          
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
-        }
-     }   
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        }         
+     },
+     computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+        onSale() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        }         
+     }        
  })
  
