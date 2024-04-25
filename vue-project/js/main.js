70e4e94134f4e24bae5e23781061fc57ad9af1a3
ekzofallendreamer <theskeletrongame@gmail.com>
2024-04-25 13:49:43 +0700

    final task 11

--- vue-project/js/main.js
+++ vue-project/js/main.js
@@ -1,3 +1,97 @@
+Vue.component('product-review', {
+    template: `
+    
+    <form class="review-form" @submit.prevent="onSubmit">
+    <p v-if="errors.length">
+ <b>Please correct the following error(s):</b>
+ <ul>
+   <li v-for="error in errors">{{ error }}</li>
+ </ul>
+</p>
+
+ <p>
+   <label for="name">Name:</label>
+   <input id="name" v-model="name" placeholder="name">
+ </p>
+
+ <p>
+   <label for="review">Review:</label>
+   <textarea id="review" v-model="review"></textarea>
+ </p>
+
+ <p>
+   <label for="rating">Rating:</label>
+   <select id="rating" v-model.number="rating">
+     <option>5</option>
+     <option>4</option>
+     <option>3</option>
+     <option>2</option>
+     <option>1</option>
+   </select>
+ </p>
+
+ <p>
+   <input type="submit" value="Submit"> 
+ </p>
+
+ <p>
+ <label for="question">Would you recommend this product?</label>
+ <div class="radio">
+
+    <p class="radio1">
+    <input type="radio" 
+    name="question"
+    v-model="question"
+    id="Yes"
+    value="Yes">
+    Yes</p>
+    
+    <p class="radio2">
+    <input type="radio" 
+    name="question"
+    v-model="question"
+    id="No"
+    value="No">
+    No</p>
+</div>
+</p>
+
+ </form>
+  `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            errors: []
+        }
+     },
+ 
+    methods:{
+        onSubmit() {
+            if(this.name && this.review && this.rating) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
+            } else {
+                if(!this.name) this.errors.push("Name required.")
+                if(!this.review) this.errors.push("Review required.")
+                if(!this.rating) this.errors.push("Rating required.")
+            }
+         },
+         
+        addReview(productReview) {
+            this.reviews.push(productReview)
+        } 
+     }     
+})
+
 Vue.component('product', {
     props: {
         premium: {
@@ -6,8 +100,9 @@ Vue.component('product', {
         }
     },
     template: `
+    
     <div class="product">
-   
+
     <div class="product-image">
       <img :src="image" :alt="altText" />
     </div>
@@ -20,6 +115,8 @@ Vue.component('product', {
             <p v-else class="outofstock">Out of Stock</p>
       <span v-if="onSale">On Sale</span>
       <span v-else>Out of Sale</span>
+
+      <product-details></product-details>
       
       <p>Shipping: {{ shipping }}</p>
 
@@ -43,13 +140,25 @@ Vue.component('product', {
       >
          Add to cart
       </button>
-      </div
+      </div>
     </div>
+    <div>
+<h2>Reviews</h2>
+<p v-if="!reviews.length">There are no reviews yet.</p>
+<ul>
+  <li v-for="review in reviews">
+  <p>{{ review.name }}</p>
+  <p>Rating: {{ review.rating }}</p>
+  <p>{{ review.review }}</p>
+  </li>
+</ul>
+</div>
+
     
-    
 </div>
 </div>
 </div>
+
   `,
   
   
@@ -62,7 +171,7 @@ Vue.component('product', {
         description: "A pair of warm, fuzzy socks",
         altText: "A pair of warm, fuzzy socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        reviews: [],
         variants: [
             {
                 variantId: 2234,
@@ -118,8 +227,13 @@ Vue.component('product-details', {
             <li v-for="detail in details">{{ detail }}</li>
         </ul>
     `,
+    data() {
+        return {
+            details: ['80% cotton', '20% polyester', 'Gender-neutral']
+        }
+    }
 })
-
+ 
  let app = new Vue({
     el: '#app',
     data: {
